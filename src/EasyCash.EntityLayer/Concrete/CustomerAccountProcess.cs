599b74d7eb3100a0a0c1401a728742e8c171acda
Mustafa Oğuzcan Kaçmaz <oguzcankacmaz@hotmail.com>
2023-06-30 23:01:14 +0300

    CustomerAccount - CustomerAccountProcess Entityleri Oluşturuldu

---
+++ src/EasyCash.EntityLayer/Concrete/CustomerAccountProcess.cs
@@ -0,0 +1,10 @@
+﻿namespace EasyCash.EntityLayer.Concrete;
+
+public class CustomerAccountProcess
+{
+    public int CustomerAccountProcessID { get; set; }
+    public string ProcessType { get; set; }
+    public decimal Amount { get; set; }
+    public DateTime ProcessDate { get; set; }
+
+}
